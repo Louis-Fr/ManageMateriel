fc5e7a487ab1e26273151b7235b746b899ede9e6
Louis Rey <louisreypro@yahoo.com>
2022-12-30 09:06:16 +0100

    Initial Commit

---
+++ Materiel2/Materiel2App.swift
@@ -0,0 +1,17 @@
+//
+//  Materiel2App.swift
+//  Materiel2
+//
+//  Created by Louis REY on 30/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Materiel2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
